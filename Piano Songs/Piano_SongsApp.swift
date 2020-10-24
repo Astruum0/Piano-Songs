c0b8785f77331b0239f3c35d8c9ff77251b3c5c3
arthur.vella@ynov.com <arthur.vella@ynov.com>
2020-10-24 21:08:49 +0200

    Initial Commit

---
+++ Piano Songs/Piano_SongsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Piano_SongsApp.swift
+//  Piano Songs
+//
+//  Created by Arthur VELLA on 24/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Piano_SongsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
